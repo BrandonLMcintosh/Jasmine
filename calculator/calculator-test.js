efef78d503512455c15ae59d65fc13f2c4108b44
Brandon McIntosh <Brandon.L.Mcintosh@gmail.com>
2020-10-09 14:32:48 -0700

    Finished setting up functions. Implemented monthly payment and jasmine tests.

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -1,11 +1,46 @@
 
 it('should calculate the monthly rate correctly', function () {
-  // ...
+  let value1 = {
+    amount: 10000, 
+    years: 10, 
+    rate: 5.0}
+  let value2 = {
+    amount: 0, 
+    years: 10, 
+    rate: 1.5}
+  expect(calculateMonthlyPayment(value1)).toEqual(106.07);
+  expect(calculateMonthlyPayment(value2)).toEqual(0.00);
 });
 
 
 it("should return a result with 2 decimal places", function() {
-  // ..
+  let value1 = {
+    amount: 15000, 
+    years: 4, 
+    rate: 7.8
+  }
+  let value2 = {
+    amount: 0, 
+    years: 5, 
+    rate: 8.0
+  }
+  expect(calculateMonthlyPayment(value1)).toEqual(364.79);
+  expect(calculateMonthlyPayment(value2)).toEqual(0.00);
+});
+
+it('should handle multi-digit integer rates', function(){
+  let value1 = {
+    amount: 10000,
+    years: 10,
+    rate: 100
+  }
+  let value2 = {
+    amount: 1000,
+    years: 10, 
+    rate: 500
+  }
+  expect(calculateMonthlyPayment(value1)).toEqual(833.39);
+  expect(calculateMonthlyPayment(value2)).toEqual(4166.67);
 });
 
 /// etc
--- calculator/calculator.js
+++ calculator/calculator.js
@@ -17,29 +17,51 @@ function getCurrentUIValues() {
   }
 }
 
-// monthly payment=(P×i)/(1−(1+i)^n)
-// P = Amount of principle
-// i = periodic interest rate (in our case yearly rate ÷ 12)
-// n = total number of payments (years × 12)
-
 // Get the inputs from the DOM.
 // Put some default values in the inputs
 // Call a function to calculate the current monthly payment
 function setupIntialValues() {
+  let amount = document.querySelector('#loan-amount');
+  let years = document.querySelector('#loan-years');
+  let rate = document.querySelector('#loan-rate');
+  amount.value = 15000;
+  years.value = 5;
+  rate.value = 7.9;
+  // numAmount = Number(amount.value);
+  // numYears = Number(years.value);
+  // numRate = Number(rate.value);
+  let decimalRate = rate.value / 100;
+  value = {amount: amount.value, years: years.value, rate: decimalRate}
+  updateMonthly(calculateMonthlyPayment(value));
 }
 
 // Get the current values from the UI
 // Update the monthly payment
 function update() {
+  let value = getCurrentUIValues();
+  value.rate = value.rate / 100;
+  updateMonthly(calculateMonthlyPayment(value));
 }
 
 // Given an object of values (a value has amount, years and rate ),
 // calculate the monthly payment.  The output should be a string
 // that always has 2 decimal places.
-function calculateMonthlyPayment(values) {
+function calculateMonthlyPayment(value) {
+// monthly payment=(P×i)/(1−(1+i)^n)
+// P = Amount of principle
+// i = periodic interest rate (in our case yearly rate ÷ 12)
+// n = total number of payments (years × 12)
+  let periodicInterest = value.rate / 12;
+  let numPayments = value.years * 12;
+  let monthlyPayment = ((value.amount * periodicInterest)/(1-(1+periodicInterest)**(numPayments * -1))).toFixed(2);
+  console.log(monthlyPayment);
+  return monthlyPayment;
+  // return ((amount * periodicInterest)/(1-Math.pow((1+periodicInterest), numOfPayments)));
 }
 
 // Given a string representing the monthly payment value,
 // update the UI to show the value.
 function updateMonthly(monthly) {
+  let monthlyPayment = document.querySelector('#monthly-payment');
+  monthlyPayment.innerText = monthly;
 }
