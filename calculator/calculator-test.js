e8cbd96211bce6f9759be3c5fc7351b341dd24dc
Brandon McIntosh <Brandon.L.Mcintosh@gmail.com>
2020-10-10 13:44:10 -0700

    added additional tests to calculator

--- calculator/calculator-test.js
+++ calculator/calculator-test.js
@@ -4,28 +4,51 @@ it('should calculate the monthly rate correctly', function () {
     amount: 10000, 
     years: 10, 
     rate: 5.0}
-  let value2 = {
-    amount: 0, 
-    years: 10, 
-    rate: 1.5}
   expect(calculateMonthlyPayment(value1)).toEqual(106.07);
-  expect(calculateMonthlyPayment(value2)).toEqual(0.00);
 });
 
+it('should display 0 for $0 amounts', function(){
+  let value1 = {
+    amount: 0,
+    years: 10,
+    rate: 5.0
+  };
+  expect(calculateMonthlyPayment(value1)).toEqual(0);
+});
 
+it('should display simple (amount/years)/12 for rates = 0', function(){
+  let value1 = {
+    amount: 10000,
+    years: 10,
+    rate: 0
+  };
+  expect(calculateMonthlyPayment(value1)).toEqual(83.33);
+});
+
+it('should display (f)ull amount + full interest)/12 for years = 0', function(){
+  let value1 = {
+    amount: 10000,
+    years: 0,
+    rate: 10
+  }
+  expect(calculateMonthlyPayment(value1)).toEqual(10100);
+});
+
+it('should display amount/12 for years = 0 and rate = 0', function(){
+  let value1 = {
+    amount: 10000, 
+    years: 0, 
+    rate: 0
+  };
+  expect(calculateMonthlyPayment(value1)).toEqual(10000);
+})
 it("should return a result with 2 decimal places", function() {
   let value1 = {
     amount: 15000, 
     years: 4, 
     rate: 7.8
   }
-  let value2 = {
-    amount: 0, 
-    years: 5, 
-    rate: 8.0
-  }
   expect(calculateMonthlyPayment(value1)).toEqual(364.79);
-  expect(calculateMonthlyPayment(value2)).toEqual(0.00);
 });
 
 it('should handle multi-digit integer rates', function(){
@@ -34,13 +57,5 @@ it('should handle multi-digit integer rates', function(){
     years: 10,
     rate: 100
   }
-  let value2 = {
-    amount: 1000,
-    years: 10, 
-    rate: 500
-  }
   expect(calculateMonthlyPayment(value1)).toEqual(833.39);
-  expect(calculateMonthlyPayment(value2)).toEqual(4166.67);
 });
-
-/// etc
